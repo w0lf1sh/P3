f064af4b2aa77eddd8bdf22cf3c8b4bbe8d81e79
Albino Nogueiras Rodríguez <albino.nogueiras@upc.edu>
2021-03-22 17:25:52 +0100

    Ficheros originales de la práctica P3

---
+++ scripts/run_get_pitch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Put here the program (maybe with path)
+GETF0="get_pitch"
+
+for fwav in pitch_db/train/*.wav; do
+    ff0=${fwav/.wav/.f0}
+    echo "$GETF0 $fwav $ff0 ----"
+    $GETF0 $fwav $ff0 > /dev/null || (echo "Error in $GETF0 $fwav $ff0"; exit 1)
+done
+
+exit 0
